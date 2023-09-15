fixed treasury return calculation for <1y duration

funcs.py
@@ -67,8 +67,8 @@ def load_us_treasury_rate(duration: Literal['1MO', '3MO', '6MO', '1', '2', '3', 
 
 def load_us_treasury_returns(duration: Literal['1MO', '3MO', '6MO', '1', '2', '3', '5', '7', '10', '20', '30']):
     treasury = load_us_treasury_rate(duration)
-    treasury['old_issue_start_price'] = treasury['rate'].div(100).add(1).pow(-int(duration)).shift()
-    treasury['old_issue_end_price'] = treasury['rate'].div(100).add(1).pow(1 / 365 - int(duration))
+    treasury['old_issue_start_price'] = treasury['rate'].div(100).add(1).pow(-eval(duration.replace('MO', '/12'))).shift()
+    treasury['old_issue_end_price'] = treasury['rate'].div(100).add(1).pow(1 / 365 - eval(duration.replace('MO', '/12')))
     treasury['change'] = treasury['old_issue_end_price'].div(treasury['old_issue_start_price'])
     treasury['price'] = np.exp(np.log(treasury['change']).cumsum()).fillna(1)
     