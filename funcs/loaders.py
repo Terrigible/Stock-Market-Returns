dec9561202cb1795d04bc812c8c50909bd7948cd
Terrigible <43108446+Terrigible@users.noreply.github.com>
2024-02-12 13:32:57 +0800

    fix(funcs/loaders.py): shift index of shiller S&P 500 data to end of business month

--- funcs/loaders.py
+++ funcs/loaders.py
@@ -110,7 +110,7 @@ def load_us_treasury_returns(duration: Literal['1MO', '3MO', '6MO', '1', '2', '3
 
 def read_shiller_sp500_data(net=False):
     df = pd.read_excel('data/ie_data.xls', 'Data', skiprows=range(7), skipfooter=1).drop(['Unnamed: 13', 'Unnamed: 15'], axis=1)
-    df['Date'] = df['Date'].apply(lambda x: pd.to_datetime(f'{x:.2f}', format='%Y.%m'))
+    df['Date'] = df['Date'].apply(lambda x: pd.to_datetime(f'{x:.2f}', format='%Y.%m')).add(BMonthEnd(0))
     df = df.set_index('Date')
     shiller_sp500 = df['P'].add(df['D'].ffill().div(12).mul(0.7 if net else 1)).div(df['P'].shift(1)).fillna(1).cumprod()
     shiller_sp500 = pd.DataFrame(shiller_sp500.rename_axis('date').rename('price'))
