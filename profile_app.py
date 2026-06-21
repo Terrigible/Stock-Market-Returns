0d47ce4a33cb23fdb693d56925f10f36a0a50bb1
Terrigible <43108446+Terrigible@users.noreply.github.com>
2026-06-21 12:57:41 +0800

    feat(profile_app.py): add profiling setup and configure app to run in non-threaded mode

--- profile_app.py
+++ profile_app.py
@@ -1,9 +1,14 @@
+import os
+
 from werkzeug.middleware.profiler import ProfilerMiddleware
 
 from returns_dashboard import app
 
 if __name__ == "__main__":
+    os.makedirs("./profiles", exist_ok=True)
+    with open("profiles/.gitignore", "w") as f:
+        f.write("*\n")
     app.server.wsgi_app = ProfilerMiddleware(
         app.server.wsgi_app, restrictions=[30], profile_dir="./profiles"
     )
-    app.run()
+    app.run(threaded=False)
